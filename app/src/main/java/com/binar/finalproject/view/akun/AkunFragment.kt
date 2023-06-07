7ad93a2256b51408e7fc6540dc2b3d3c51dc8a84
FaridRifaan <faridrifan59@gmail.com>
2023-06-07 21:28:17 +0700

    Add riwayat page and list riwayat after login

--- app/src/main/java/com/binar/finalproject/view/akun/AkunFragment.kt
+++ app/src/main/java/com/binar/finalproject/view/akun/AkunFragment.kt
@@ -1,4 +1,4 @@
-package com.binar.finalproject.view.riwayat
+package com.binar.finalproject.view.akun
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [RiwayatNonLoginFragment.newInstance] factory method to
+ * Use the [AkunFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class RiwayatNonLoginFragment : Fragment() {
+class AkunFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -35,7 +35,7 @@ class RiwayatNonLoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_riwayat_non_login, container, false)
+        return inflater.inflate(R.layout.fragment_akun, container, false)
     }
 
     companion object {
@@ -45,12 +45,12 @@ class RiwayatNonLoginFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment RiwayatNonLoginFragment.
+         * @return A new instance of fragment AkunFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            RiwayatNonLoginFragment().apply {
+            AkunFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
