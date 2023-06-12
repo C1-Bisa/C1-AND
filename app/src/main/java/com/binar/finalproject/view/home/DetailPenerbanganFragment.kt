63cb47b94c5add88954846af892bf0ed024e4b36
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-06-12 12:47:03 +0700

    add binding and bottom sheet dialog

--- app/src/main/java/com/binar/finalproject/view/home/DetailPenerbanganFragment.kt
+++ app/src/main/java/com/binar/finalproject/view/home/DetailPenerbanganFragment.kt
@@ -1,60 +1,54 @@
 package com.binar.finalproject.view.home
 
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.binar.finalproject.R
+import com.binar.finalproject.databinding.DialogTicketSoldOutBinding
+import com.binar.finalproject.databinding.FragmentDetailPenerbanganBinding
+import com.binar.finalproject.databinding.SeatclassDialogLayoutBinding
+import com.google.android.material.bottomsheet.BottomSheetDialog
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [DetailPenerbanganFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DetailPenerbanganFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
+    private lateinit var binding : FragmentDetailPenerbanganBinding
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_penerbangan, container, false)
+        binding = FragmentDetailPenerbanganBinding.inflate(layoutInflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment DetailPenerbanganFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DetailPenerbanganFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.btnSelectFlight.setOnClickListener {
+//            jika ticket habis show dialog
+            showDialogTecketSoldOut()
+        }
     }
+
+    private fun showDialogTecketSoldOut() {
+        val dialog = BottomSheetDialog(requireContext())
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.dialog_ticket_sold_out)
+        val bindingDialog = DialogTicketSoldOutBinding.inflate(layoutInflater)
+        dialog.setContentView(bindingDialog.root)
+
+        bindingDialog.btnClose.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT));
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation;
+        dialog.window?.setGravity(Gravity.BOTTOM);
+    }
+
 }
