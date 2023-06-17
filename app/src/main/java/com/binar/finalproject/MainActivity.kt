2e153dd9c7a0c0c6e66f1f26150ccb70fa764f07
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-06-17 18:16:18 +0700

    set bottom nav using navigation component

--- app/src/main/java/com/binar/finalproject/MainActivity.kt
+++ app/src/main/java/com/binar/finalproject/MainActivity.kt
@@ -2,12 +2,23 @@ package com.binar.finalproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.binar.finalproject.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
     }
 }
