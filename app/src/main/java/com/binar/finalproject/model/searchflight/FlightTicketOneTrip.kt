32318f0d0af3bfa2a927fd117d75940c3174cd85
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-06-22 00:22:19 +0700

    add data class

---
+++ app/src/main/java/com/binar/finalproject/model/searchflight/FlightTicketOneTrip.kt
@@ -0,0 +1,5 @@
+package com.binar.finalproject.model.searchflight
+
+data class FlightTicketOneTrip(
+    var flightIdDeparture : Int = 0
+)
---
+++ app/src/main/java/com/binar/finalproject/model/searchflight/FlightTicketRoundTrip.kt
@@ -0,0 +1,6 @@
+package com.binar.finalproject.model.searchflight
+
+data class FlightTicketRoundTrip(
+    var flightIdDeparture : Int = 0,
+    var flightIdReturn : Int = 0
+) : java.io.Serializable
