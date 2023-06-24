b6e91887eacd8f0386c56d1bf13289883f415b38
muhammadhilmisatrio <hilmisatrio16@gmail.com>
2023-06-25 01:00:34 +0700

    add serializable

--- app/src/main/java/com/binar/finalproject/model/bio/BiodataPemesan.kt
+++ app/src/main/java/com/binar/finalproject/model/bio/BiodataPemesan.kt
@@ -2,7 +2,7 @@ package com.binar.finalproject.model.bio
 
 data class BiodataPemesan(
     var fullName : String,
-    var nameClan : String,
+    var familyName : String,
     var phoneNumber : String,
     var email : String
-)
+) : java.io.Serializable
