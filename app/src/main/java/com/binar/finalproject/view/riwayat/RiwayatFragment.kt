1bd66588bad9d90cf5dbeb250190eaa105fb82bf
FaridRifaan <faridrifan59@gmail.com>
2023-07-01 00:42:20 +0700

    add bottom navigation visible, loading screen, and descending list data history by id transaction

--- app/src/main/java/com/binar/finalproject/view/riwayat/RiwayatFragment.kt
+++ app/src/main/java/com/binar/finalproject/view/riwayat/RiwayatFragment.kt
@@ -18,6 +18,7 @@ import com.binar.finalproject.databinding.SearchDialogRiwayatBinding
 import com.binar.finalproject.local.DataStoreUser
 import com.binar.finalproject.view.adapter.AdapterRiwayat
 import com.binar.finalproject.viewmodel.TransactionHistoryViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.squareup.timessquare.CalendarPickerView
 import dagger.hilt.android.AndroidEntryPoint
@@ -48,6 +49,11 @@ class RiwayatFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val bottomNav = requireActivity().findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNav.visibility = View.VISIBLE
+
+        //untuk meload data
+        binding.layoutLoadingData.visibility = View.VISIBLE
 
         dataSotreUser = DataStoreUser(requireContext().applicationContext)
 
@@ -74,9 +80,13 @@ class RiwayatFragment : Fragment() {
         if (dataSotreUser.isAlreadyLogin()) {
             binding.layoutNonLogin.visibility = View.GONE
             binding.linearLayoutLogin.visibility = View.VISIBLE
+
+
         } else {
             binding.layoutNonLogin.visibility = View.VISIBLE
             binding.linearLayoutLogin.visibility = View.GONE
+
+
 
         }
 
@@ -104,6 +114,8 @@ class RiwayatFragment : Fragment() {
         dialogFilterDate.window?.attributes?.windowAnimations = R.style.DialogAnimation;
         dialogFilterDate.window?.setGravity(Gravity.BOTTOM);
     }
+
+
 
     private fun setInitialDatePicker(bindingDialogFilter : DateDialogRiwayatBinding) {
         val startDate = Date()
@@ -141,19 +153,27 @@ class RiwayatFragment : Fragment() {
     private fun setLayoutListData(token: String) {
         riwayatAdapter = AdapterRiwayat(ArrayList())
         transactionHistoryVM.getHistoryTransaction(token)
-        transactionHistoryVM.historyTransaction.observe(viewLifecycleOwner){
-            if (it != null && it.isNotEmpty()){
+        transactionHistoryVM.historyTransaction.observe(viewLifecycleOwner){ list ->
+            if (list != null && list.isNotEmpty()){
+                val listSorted = list.sortedByDescending { it.transaction.flights[0].transactionFlight.transactionId }
+
                 binding.rvRiwayatAfterData.apply {
                     layoutManager = LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false)
                     adapter = riwayatAdapter
                 }
 
-                riwayatAdapter.setList(it)
-                Log.i("DATA HISTORY", it.toString())
+                riwayatAdapter.setList(listSorted)
+                Log.i("DATA HISTORY", list.toString())
+
+                binding.layoutLoadingData.visibility = View.GONE
+                binding.rvRiwayatAfterData.visibility = View.VISIBLE
 
             }else{
                 binding.rvRiwayatAfterData.visibility = View.GONE
                 binding.layoutRiwayatEmpty.visibility = View.VISIBLE
+
+                binding.layoutLoadingData.visibility = View.GONE
+
             }
 
         }
@@ -162,6 +182,8 @@ class RiwayatFragment : Fragment() {
                 val bundleIdTransaction = Bundle().apply {
                     putInt("ID_TRANSACTION", it.transaction.flights[0].transactionFlight.transactionId)
                 }
+                binding.layoutLoadingData.visibility = View.GONE
+
                 findNavController().navigate(R.id.action_riwayatFragment_to_detailRiwayatFragment, bundleIdTransaction)
                 Log.i("BUNDLE_ID_TRANSACTION", it.transaction.flights[0]?.transactionFlight?.transactionId.toString())
 
