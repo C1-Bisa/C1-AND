27760b5a254e1f41cababbe3685982c2f417df8a
FaridRifaan <faridrifan59@gmail.com>
2023-07-03 16:04:38 +0700

    delete code

--- app/src/main/java/com/binar/finalproject/viewmodel/TransactionHistoryViewModel.kt
+++ app/src/main/java/com/binar/finalproject/viewmodel/TransactionHistoryViewModel.kt
@@ -17,8 +17,6 @@ import javax.inject.Inject
 @HiltViewModel
 class TransactionHistoryViewModel @Inject constructor(private val api : RestfulApi) : ViewModel(){
 
-    private val _detailHistory = MutableLiveData<com.binar.finalproject.model.gethistory.detailhistory.Data?>()
-    val detailHistory : LiveData<com.binar.finalproject.model.gethistory.detailhistory.Data?> = _detailHistory
 
     private val _historyTransaction = MutableLiveData<List<Data>?>()
     val historyTransaction : LiveData<List<Data>?> = _historyTransaction
@@ -44,26 +42,7 @@ class TransactionHistoryViewModel @Inject constructor(private val api : RestfulA
 
         })
     }
-//    fun detailHistoryTransaction(data : PostTransactionHistory){
-//        api.getDetailTransactionHistory(data).enqueue(object : Callback<ResponseDetailTransactionHistory>{
-//            override fun onResponse(
-//                call: Call<ResponseDetailTransactionHistory>,
-//                response: Response<ResponseDetailTransactionHistory>
-//            ) {
-//                if (response.isSuccessful){
-//                    _detailHistory.postValue(response.body()!!.data)
-//                }else{
-//                    _detailHistory.postValue(null)
-//                }
-//            }
-//
-//            override fun onFailure(call: Call<ResponseDetailTransactionHistory>, t: Throwable) {
-//                _detailHistory.postValue(null)
-//
-//            }
-//
-//        })
-//    }
+
 
 
 }
